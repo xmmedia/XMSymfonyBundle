e92271e4123e036ab9ea91b89d9e2e49b81dcb19
Darryl Hein <dhein@xmmedia.com>
2019-09-29 23:16:47 -0600

    namespace fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\DependencyInjection;
+namespace Xm\SymfonyBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
