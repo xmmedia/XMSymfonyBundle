3dea077cdbbaadfb095c54d21998833dcea29005
Darryl Hein <dhein@xmmedia.com>
2019-12-06 11:38:33 -0700

    add date gql type

---
+++ Infrastructure/GraphQl/Type/DateType.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xm\SymfonyBundle\Infrastructure\GraphQl\Type;
+
+use GraphQL\Language\AST\StringValueNode;
+use GraphQL\Type\Definition\ScalarType;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+
+final class DateType extends ScalarType implements AliasedInterface
+{
+    private const NAME = 'Date';
+
+    public function __construct()
+    {
+        parent::__construct([
+            'name'        => self::NAME,
+            'description' => 'Date represented as string, the format of Y-m-d.',
+        ]);
+    }
+
+    /**
+     * @param \DateTimeInterface $value
+     */
+    public function serialize($value): ?string
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        if (\is_string($value)) {
+            return $value;
+        }
+
+        return $value->format('Y-m-d');
+    }
+
+    /**
+     * @param string $value
+     */
+    public function parseValue($value): ?\DateTimeImmutable
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        return new \DateTimeImmutable($value);
+    }
+
+    /**
+     * @param StringValueNode $valueNode
+     */
+    public function parseLiteral($valueNode, array $variables = null): ?\DateTimeImmutable
+    {
+        if (!$valueNode instanceof StringValueNode) {
+            return null;
+        }
+
+        return $this->parseValue($valueNode->value);
+    }
+
+    public static function getAliases(): array
+    {
+        return [self::NAME];
+    }
+}
