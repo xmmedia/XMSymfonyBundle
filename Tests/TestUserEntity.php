2d3bcb46ded3df42c362e579fcab361b5d1a8f5a
Darryl Hein <dhein@xmmedia.com>
2019-12-18 15:11:47 -0700

    test user entity should implement userinterface

--- Tests/TestUserEntity.php
+++ Tests/TestUserEntity.php
@@ -4,12 +4,38 @@ declare(strict_types=1);
 
 namespace Xm\SymfonyBundle\Tests;
 
-class TestUserEntity
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class TestUserEntity implements UserInterface
 {
     private $userId;
 
     public function userId()
     {
         return $this->userId;
+    }
+
+    public function getRoles()
+    {
+        return [];
+    }
+
+    public function getPassword()
+    {
+        return '';
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function getUsername()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
     }
 }
