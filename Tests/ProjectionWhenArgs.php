6c2719a1abfa4f518e4712008856a4e6c918e053
Darryl Hein <dhein@xmmedia.com>
2020-11-17 13:43:44 -0700

    add trait to help testing projections

---
+++ Tests/ProjectionWhenArgs.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xm\SymfonyBundle\Tests;
+
+trait ProjectionWhenArgs
+{
+    private function whenArgs(array $projectedEvents)
+    {
+        return function ($eventHandlers) use ($projectedEvents) {
+            if (!\is_array($eventHandlers)) {
+                return false;
+            }
+
+            // make sure all events in list are used
+            foreach ($projectedEvents as $event) {
+                if (!\array_key_exists($event, $eventHandlers)) {
+                    return false;
+                }
+            }
+
+            // make sure there are not extra events
+            foreach ($eventHandlers as $event => $handler) {
+                if (false === array_search($event, $projectedEvents)) {
+                    return false;
+                }
+            }
+
+            return true;
+        };
+    }
+}
