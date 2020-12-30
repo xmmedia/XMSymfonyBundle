3bd23055e6f22ea45b58383e5f951a993819c126
Darryl Hein <dhein@xmmedia.com>
2020-12-30 16:53:51 -0700

    add collection for value objects

---
+++ Model/ValueObjectCollection.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xm\SymfonyBundle\Model;
+
+/**
+ * Collection built for use with classes the implement the ValueObject interface,
+ * including UUIDs.
+ */
+abstract class ValueObjectCollection extends Collection
+{
+    /** @var ValueObject[] */
+    protected $data = [];
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param ValueObject $element
+     */
+    public function contains($element, bool $strict = true): bool
+    {
+        foreach ($this->data as $item) {
+            if ($item->sameValueAs($element)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param ValueObject $element
+     */
+    public function remove($element): bool
+    {
+        foreach ($this->data as $i => $item) {
+            if ($item->sameValueAs($element)) {
+                unset($this->data[$i]);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
