4bec39b7d399ae42b40eb4e958b1e017c8d6d67a
Darryl Hein <dhein@xmmedia.com>
2021-01-26 21:20:14 -0700

    add entrypoint integrity hash lookup
    also adds the webpack encore bundle as a requirement

---
+++ Infrastructure/GraphQl/Resolver/EntrypointIntegrityHashResolver.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xm\SymfonyBundle\Infrastructure\GraphQl\Resolver;
+
+use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupCollectionInterface;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupInterface;
+
+class EntrypointIntegrityHashResolver implements ResolverInterface
+{
+    /** @var EntrypointLookupInterface */
+    private $entrypointLookup;
+
+    public function __construct(EntrypointLookupCollectionInterface $collection)
+    {
+        $this->entrypointLookup = $collection->getEntrypointLookup();
+    }
+
+    public function __invoke(string $entrypoint): ?string
+    {
+        $path = $this->entrypointLookup->getJavaScriptFiles($entrypoint)[0];
+        $hashes = $this->entrypointLookup->getIntegrityData();
+
+        if (empty($hashes)) {
+            return null;
+        }
+
+        return $hashes[$path];
+    }
+}
