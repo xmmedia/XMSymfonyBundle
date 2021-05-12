142a84037eed93658af6bc385e8f7b1fc295f56c
Darryl Hein <dhein@xmmedia.com>
2021-05-11 21:29:28 -0600

    add ProjectionAwareMigration trait

---
+++ Migrations/ProjectionAwareMigration.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xm\SymfonyBundle\Migrations;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+trait ProjectionAwareMigration
+{
+    /** @var ContainerInterface */
+    private $container;
+
+    protected function rebuildProjection(string $projection): void
+    {
+        if (!isset($this->container)) {
+            throw new \RuntimeException(sprintf('The migration must implement the %s class to use %s', ContainerAwareInterface::class, self::class));
+        }
+
+        $this->write('Rebuilding projection: '.$projection);
+
+        $application = new Application($this->container->get('kernel'));
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command'         => 'app:projection:rebuild',
+            'projection-name' => $projection,
+        ]);
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        $this->write($output->fetch());
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        if (null === $container) {
+            throw new \RuntimeException('The container must be set for the migration to work. NULL received.');
+        }
+
+        $this->container = $container;
+    }
+}
