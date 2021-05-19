b73b289a96370d6cc61b5201799f7b414d4e5de9
Darryl Hein <dhein@xmmedia.com>
2021-05-19 13:34:28 -0600

    add fake provider for emailGatewayMessageId

--- DataFixtures/Faker/Provider/UuidFakerProvider.php
+++ DataFixtures/Faker/Provider/UuidFakerProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Xm\SymfonyBundle\DataFixtures\Faker\Provider;
 
 use Faker;
+use Xm\SymfonyBundle\Model\EmailGatewayMessageId;
 use Xm\SymfonyBundle\Tests\FakeId;
 
 /**
@@ -15,5 +16,10 @@ class UuidFakerProvider extends Faker\Provider\Uuid
     public function fakeId(): FakeId
     {
         return FakeId::fromString(parent::uuid());
+    }
+
+    public function emailGatewayMessageId(): EmailGatewayMessageId
+    {
+        return EmailGatewayMessageId::fromString(parent::uuid());
     }
 }
