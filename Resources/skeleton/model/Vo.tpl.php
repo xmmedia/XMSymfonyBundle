84cafa29aaf96751e10edec272140c466f23545a
Darryl Hein <dhein@xmmedia.com>
2024-02-24 21:29:42 -0700

    switch to self as class is VO

--- Resources/skeleton/model/Vo.tpl.php
+++ Resources/skeleton/model/Vo.tpl.php
@@ -47,7 +47,7 @@ final readonly class <?= $class_name; ?> implements ValueObject
 
     public function sameValueAs(self|ValueObject $other): bool
     {
-        if (static::class !== $other::class) {
+        if (self::class !== $other::class) {
             return false;
         }
 
