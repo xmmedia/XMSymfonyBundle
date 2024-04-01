df48fe6ff7556b9a2e56735c353a1502ed6b27a9
Darryl Hein <dhein@xmmedia.com>
2024-04-01 17:13:20 -0600

    remove unneeded variable

--- Model/Country.php
+++ Model/Country.php
@@ -29,7 +29,7 @@ class Country implements ValueObject
         try {
             Assert::length($abbreviation, 2);
             Assert::oneOf($abbreviation, CountryProvider::abbreviations());
-        } catch (\InvalidArgumentException $e) {
+        } catch (\InvalidArgumentException) {
             throw InvalidCountry::invalid($abbreviation);
         }
 
