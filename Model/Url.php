a73fc93be3c3c6904fb2ebcba7db6b89da35d836
Darryl Hein <dhein@xmmedia.com>
2024-04-01 17:12:57 -0600

    add URL VO

---
+++ Model/Url.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xm\SymfonyBundle\Model;
+
+use Xm\SymfonyBundle\Util\Assert;
+use Xm\SymfonyBundle\Util\StringUtil;
+
+class Url implements ValueObject
+{
+    public const MAX_LENGTH = 2000;
+
+    private readonly string $url;
+
+    public static function fromString(string $url): static
+    {
+        return new static($url);
+    }
+
+    private function __construct(string $url)
+    {
+        $url = StringUtil::trim($url);
+
+        Assert::notEmpty($url);
+        Assert::url($url);
+        Assert::maxLength($url, static::MAX_LENGTH);
+
+        $this->url = $url;
+    }
+
+    public function url(): string
+    {
+        return $this->url;
+    }
+
+    public function toString(): string
+    {
+        return $this->url();
+    }
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+    /**
+     * @param static|ValueObject $other
+     */
+    public function sameValueAs(ValueObject $other): bool
+    {
+        if (static::class !== $other::class) {
+            return false;
+        }
+
+        return $this->url === $other->url;
+    }
+}
