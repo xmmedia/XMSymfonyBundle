c389dfd7324b3483c2c39a9c37e229fede69a3d5
Darryl Hein <dhein@xmmedia.com>
2025-11-05 09:22:10 -0700

    ID VOs are stringable

--- Resources/skeleton/model/ArId.tpl.php
+++ Resources/skeleton/model/ArId.tpl.php
@@ -8,7 +8,7 @@ use Xm\SymfonyBundle\Model\UuidId;
 use Xm\SymfonyBundle\Model\UuidInterface;
 use Xm\SymfonyBundle\Model\ValueObject;
 
-final class <?= $class_name; ?> implements ValueObject, UuidInterface
+final class <?= $class_name; ?> implements ValueObject, UuidInterface, \Stringable
 {
     use UuidId;
 }
