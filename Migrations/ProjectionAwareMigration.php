7139588d39b453f2176ee42147c871e576418957
Darryl Hein <dhein@xmmedia.com>
2026-05-29 08:33:53 -0600

    add createEventStream & runProjection to ProjectionAwareMigration

--- Migrations/ProjectionAwareMigration.php
+++ Migrations/ProjectionAwareMigration.php
@@ -20,6 +20,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *     // ...
  *     public function postUp(Schema $schema): void
  *     {
+ *          $this->createEventStream('user');
+ *          $this->runProjection('user');
  *          $this->rebuildProjection('user');
  *     }
  * }
@@ -30,37 +32,66 @@ trait ProjectionAwareMigration
 
     abstract public function write(string $message): void;
 
+    protected function createEventStream(string $streamName): void
+    {
+        $this->write('Creating event stream: '.$streamName);
+
+        $this->runCommand(
+            [
+                'command'     => 'event-store:event-stream:create',
+                'stream_name' => $streamName,
+            ],
+            'Creating event stream failed!',
+        );
+    }
+
+    protected function runProjection(string $projection): void
+    {
+        $this->write('Running projection: '.$projection);
+
+        $this->runCommand(
+            [
+                'command'         => 'event-store:projection:run',
+                'projection-name' => $projection,
+                '--run-once'      => true,
+            ],
+            'Running projection failed!',
+        );
+    }
+
     protected function rebuildProjection(string $projection): void
+    {
+        $this->write('Rebuilding projection: '.$projection);
+
+        ini_set('memory_limit', '-1');
+
+        $this->runCommand(
+            [
+                'command'         => 'app:projection:rebuild',
+                'projection-name' => $projection,
+            ],
+            'Rebuilding projection failed!',
+        );
+    }
+
+    private function runCommand(array $parameters, string $errorMessage): void
     {
         if (!isset($this->kernel) && !isset($this->container)) {
             throw new \RuntimeException(sprintf('The migration %s must have a $kernel property (Symfony >=6.4) or implement %s (Symfony <6.4) to rebuild projections', self::class, ContainerAwareInterface::class));
         }
 
-        $this->write('Rebuilding projection: '.$projection);
-
-        ini_set('memory_limit', '-1');
-
-        if (isset($this->kernel)) {
-            $kernel = $this->kernel;
-        } else {
-            $kernel = $this->container->get('kernel');
-        }
+        $kernel = $this->kernel ?? $this->container->get('kernel');
 
         $application = new Application($kernel);
         $application->setAutoExit(false);
 
-        $input = new ArrayInput([
-            'command'         => 'app:projection:rebuild',
-            'projection-name' => $projection,
-        ]);
-
         $output = new BufferedOutput();
-        $return = $application->run($input, $output);
+        $return = $application->run(new ArrayInput($parameters), $output);
 
         $this->write($output->fetch());
 
         if (0 !== $return) {
-            throw new \RuntimeException('Rebuilding projection failed!');
+            throw new \RuntimeException($errorMessage);
         }
     }
 
