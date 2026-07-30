9d2b8c85b77e5afa3808a1f65911c48df9643ef8
Darryl Hein <dhein@xmmedia.com>
2026-07-30 13:15:18 -0600

    correct param types in not found exception
    the finder may pass a string

--- Resources/skeleton/model/ExceptionNotFound.tpl.php
+++ Resources/skeleton/model/ExceptionNotFound.tpl.php
@@ -8,7 +8,7 @@ use <?= $id_class; ?>;
 
 final class <?= $class_name; ?> extends \InvalidArgumentException
 {
-    public static function with<?= $id_class_short; ?>(<?= $id_class_short; ?> $<?= $id_property; ?>): self
+    public static function with<?= $id_class_short; ?>(<?= $id_class_short; ?>|string $<?= $id_property; ?>): self
     {
         return new self(\sprintf('<?= $model; ?> with ID "%s" cannot be found.', $<?= $id_property; ?>));
     }
