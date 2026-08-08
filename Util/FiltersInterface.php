4ac9adc19f3d2655a47e75b8e992393f5e0f5057
Darryl Hein <dhein@xmmedia.com>
2026-08-08 14:22:41 -0600

    add isTrue to FiltersInterface

--- Util/FiltersInterface.php
+++ Util/FiltersInterface.php
@@ -6,14 +6,13 @@ namespace Xm\SymfonyBundle\Util;
 
 interface FiltersInterface
 {
-    /**
-     * @return static
-     */
     public static function fromArray(?array $filters): self;
 
     public function applied(string $field): bool;
 
     public function get(string $field): mixed;
+
+    public function isTrue(mixed $value): bool;
 
     public function toArray(): array;
 }
